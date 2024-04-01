47c992d511f785a38c0f871f28f596ff2511319b
andresdiaz153 <126519659+andresdiaz153@users.noreply.github.com>
2024-03-31 23:14:01 -0500

    Update Logica.js

--- Punto1/Logica.js
+++ Punto1/Logica.js
@@ -5,17 +5,17 @@ function sumarCuadros(arrayNumeros){
     let sum = 0;
     for(let i=0; i<arrayNumeros.length; i++){
         const num = arrayNumeros[i];
-        const numStr = num.toString(); // Convertir el número a cadena
+        const numStr = num.toString(); 
         sum += num;
         const long = numStr.length;
 
-        console.log("+ " + '-'.repeat(long) + " +"); // Usar la longitud de la cadena
+        console.log("+ " + '-'.repeat(long) + " +"); 
         console.log("| " + num + " |");
-        console.log("+ " + "-".repeat(long) + " +"); // Usar la longitud de la cadena
+        console.log("+ " + "-".repeat(long) + " +"); 
     }
-    console.log("+ " + "=".repeat(sum.toString().length) + " +"); // Usar la longitud de la cadena convertida a número
+    console.log("+ " + "=".repeat(sum.toString().length) + " +");
     console.log("| " + sum + " |");
-    console.log("+ " + "=".repeat(sum.toString().length) + " +"); // Usar la longitud de la cadena convertida a número
+    console.log("+ " + "=".repeat(sum.toString().length) + " +"); 
 }
 
-sumarCuadros([521, 52222, 532, 4]); // Llamada a la función
+sumarCuadros([521, 52222, 532, 4]);
